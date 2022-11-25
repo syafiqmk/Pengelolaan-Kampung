680c3f12ab358450d0eda2c6b4a747b16171166d
syafiqmk <syafiqmuh.k@gmail.com>
2022-11-25 16:33:57 +0800

    add middleware

---
+++ app/Http/Middleware/IsOperator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsOperator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->check()) {
+            if(auth()->user()->role !== "Operator") {
+                return redirect()->route("402");
+            } else {
+                if(auth()->user()->status === "Waiting") {
+                    return redirect()->route("accountWait");
+                }
+            }
+        } else {
+            return redirect()->route("login");
+        }
+        return $next($request);
+    }
+}
---
+++ app/Http/Middleware/IsUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->check()) {
+            if(auth()->user()->role !== "User") {
+                return redirect()->route("402");
+            } else {
+                if(auth()->user()->status === "Waiting") {
+                    return redirect()->route("accountWait");
+                }
+            }
+        } else {
+            return redirect()->route("login");
+        }
+        return $next($request);
+    }
+}
