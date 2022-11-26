0dda058b0fa2c55acdf58d1dd6b2aa1d76a2e417
syafiqmk <syafiqmuh.k@gmail.com>
2022-11-26 11:57:11 +0800

    Make main Models & Migrations of Pengelolaan Kampung

---
+++ database/migrations/2022_11_26_023949_create_emergencies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emergencies', function (Blueprint $table) {
+            $table->id();
+            $table->text("description");
+            $table->text("latitude");
+            $table->text("longitude");
+            $table->enum("type", ["Bencana Alam", "Pemadam Kebakaran", "Ambulans", "Polisi"]);
+            $table->enum("status", ["Dilaporkan", "Proses", "Selesai"]);
+            $table->foreignId("user_id")->constrained("users");
+            $table->foreignId("village_id")->constrained("villages");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emergencies');
+    }
+};
---
+++ database/migrations/2022_11_26_035346_create_complaints_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+            $table->text("description");
+            $table->text("latitude");
+            $table->text("longitude");
+            $table->foreignId("category_id")->constrained("complaint_categories");
+            $table->foreignId("user_id")->constrained("users");
+            $table->foreignId("village_id")->constrained("villages");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complaints');
+    }
+};
