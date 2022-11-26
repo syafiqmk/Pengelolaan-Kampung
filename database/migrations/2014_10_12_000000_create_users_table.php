93a68ea9a7708319c32148166f69eaed1b6a752b
syafiqmk <syafiqmuh.k@gmail.com>
2022-11-26 10:04:23 +0800

    Update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text("address");
+            $table->enum("role", ["Admin", "Admin Kampung", "Operator", "User"]);
+            $table->enum("status", ["Waiting", "Granted"]);
             $table->rememberToken();
             $table->timestamps();
         });
